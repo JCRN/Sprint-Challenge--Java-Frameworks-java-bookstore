51b6a35e9f9e23a45f9418c1df0a1b88e6a92e01
JCRN <clemons.jason@gmail.com>
2019-09-20 15:53:43 -0700

    update

--- BookStore/src/main/java/com/lambdaschool/bookstore/repository/AuthorsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lambdaschool.bookstore.repository;
-
-import com.lambdaschool.bookstore.models.Authors;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AuthorsRepository extends PagingAndSortingRepository<Authors, Long>
-{
-}
